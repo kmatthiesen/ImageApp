9b32191a0d5965bc71980a1e9eaeca9e17f83501
Kevin <kmatthiesen8083@gmail.com>
2023-02-19 23:56:04 -0700

    fixes from JavaFX to work with maven

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -6,11 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.net.URL;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
