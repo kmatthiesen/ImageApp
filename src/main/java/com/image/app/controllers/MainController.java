593fe55e0e794880142b2ccff5a5e0d81baec083
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2023-04-04 17:38:55 -0500

    Added named images

--- src/main/java/com/image/app/controllers/MainController.java
+++ src/main/java/com/image/app/controllers/MainController.java
@@ -16,19 +16,19 @@ public class MainController {
 
         // TODO: Add images directory to resources: resources.com.image.app.images
 
-        final Image image1 = new Image(Main.class.getResourceAsStream("images/Image1.jpeg"));
+        final Image image1 = new Image(Main.class.getResourceAsStream("/images/Image1.jpeg"));
         final ImageView imv1 = new ImageView();
         imv1.setPreserveRatio(true);
         imv1.setFitHeight(100);
         imv1.setImage(image1);
 
-        final Image image2 = new Image(Main.class.getResourceAsStream("images/Image2.jpeg"));
+        final Image image2 = new Image(Main.class.getResourceAsStream("/images/Image2.jpeg"));
         final ImageView imv2 = new ImageView();
         imv2.setPreserveRatio(true);
         imv2.setFitHeight(100);
         imv2.setImage(image2);
 
-        final Image image3 = new Image(Main.class.getResourceAsStream("images/Image3.jpeg"));
+        final Image image3 = new Image(Main.class.getResourceAsStream("/images/Image3.jpeg"));
         final ImageView imv3 = new ImageView();
         imv3.setPreserveRatio(true);
         imv3.setFitHeight(100);
